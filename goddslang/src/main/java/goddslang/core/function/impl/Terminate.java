3b3f0f6a1635e3bfb6fc9f87989fa7e1ce9d9818
proman3419 <proman7268@gmail.com>
2023-01-21 20:44:29 +0100

    Merge master

--- goddslang/src/main/java/goddslang/core/function/impl/Terminate.java
+++ goddslang/src/main/java/goddslang/core/function/impl/Terminate.java
@@ -1,14 +1,17 @@
 package goddslang.core.function.impl;
 
+import goddslang.core.error.Error;
 import goddslang.core.function.Argument;
 import goddslang.core.function.Function;
+import goddslang.core.function.FunctionCall;
 import goddslang.core.model.Cell;
 
 import java.util.List;
 
 public class Terminate implements Function {
     @Override
-    public void call(Cell cell, List<Argument> arguments) {
+    public Error call(Cell cell, List<Argument> arguments, FunctionCall functionCall) {
         cell.terminate();
+        return null;
     }
 }
